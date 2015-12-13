f2ad4a651223eaba09bd64967d25c4204a1426ab
kashj22 <kjavaid22@gmail.com>
2015-12-13 15:02:53 +0000

    set up port

--- api/app.js
+++ api/app.js
@@ -16,3 +16,7 @@ mongoose.connect = 'mongodb://localhost:27017/flicpic';
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// start express listening on port 3000
+app.listen(3000);
+console.log("Express is listening on localhost:3000");
