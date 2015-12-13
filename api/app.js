5801d7837c486ab971b30ad1d81cb39d2f5c731e
kashj22 <kjavaid22@gmail.com>
2015-12-13 14:32:14 +0000

    required all of the packages

--- api/app.js
+++ api/app.js
@@ -1,1 +1,8 @@
-//beginning flicpic
+// Require all of our packages at the top
+var express        = require("express");
+var morgan         = require("morgan");
+var bodyParser     = require('body-parser');
+var path           = require('path');
+var expressLayouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
+var mongoose       = require('mongoose');
