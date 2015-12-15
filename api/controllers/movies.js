91b67ecab95138cf52d3a4f51c2f3856cd2c70e0
kashj22 <kjavaid22@gmail.com>
2015-12-15 11:19:53 +0000

    something fixed but need to figure out how to take our string and input it into our api

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -4,6 +4,8 @@ var Movie = require('../models/movie.js');
 // then create a dropdown with the collection returned by the server
 
 
+
+
 // GET
 function getAll(request, response) {
   Movie.find(function(error, criminals) {
@@ -13,63 +15,63 @@ function getAll(request, response) {
   }).select('-__v');
 }
 
-// POST
-function createCriminal(request, response) {
-  console.log('in POST');
-  console.log('body:',request.body);
+// // POST
+// function createCriminal(request, response) {
+//   console.log('in POST');
+//   console.log('body:',request.body);
 
-  var criminal = new Criminal(request.body);
+//   var criminal = new Criminal(request.body);
 
-  criminal.save(function(error) {
-    if(error) response.json({messsage: 'Could not ceate criminal b/c:' + error});
+//   criminal.save(function(error) {
+//     if(error) response.json({messsage: 'Could not ceate criminal b/c:' + error});
 
-    response.json({criminal: criminal});
-  });
-}
+//     response.json({criminal: criminal});
+//   });
+// }
 
-// GET
-function getCriminal(request, response) {
-  var id = request.params.id;
+// // GET
+// function getCriminal(request, response) {
+//   var id = request.params.id;
 
-  Criminal.findById({_id: id}, function(error, criminal) {
-    if(error) response.json({message: 'Could not find criminal b/c:' + error});
+//   Criminal.findById({_id: id}, function(error, criminal) {
+//     if(error) response.json({message: 'Could not find criminal b/c:' + error});
 
-    response.json({criminal: criminal});
-  }).select('-__v');
-}
+//     response.json({criminal: criminal});
+//   }).select('-__v');
+// }
 
-function updateCriminal(request, response) {
-  var id = request.params.id;
+// function updateCriminal(request, response) {
+//   var id = request.params.id;
 
-  Criminal.findById({_id: id}, function(error, criminal) {
-    if(error) response.json({message: 'Could not find criminal b/c:' + error});
+//   Criminal.findById({_id: id}, function(error, criminal) {
+//     if(error) response.json({message: 'Could not find criminal b/c:' + error});
 
-    if(request.body.name) criminal.name = request.body.name;
-    if(request.body.location) criminal.location = request.body.location;
-    if(request.body.status) criminal.status = request.body.status;
+//     if(request.body.name) criminal.name = request.body.name;
+//     if(request.body.location) criminal.location = request.body.location;
+//     if(request.body.status) criminal.status = request.body.status;
 
-    criminal.save(function(error) {
-      if(error) response.json({messsage: 'Could not update criminal b/c:' + error});
+//     criminal.save(function(error) {
+//       if(error) response.json({messsage: 'Could not update criminal b/c:' + error});
 
-      response.json({message: 'Criminal successfully updated', criminal: criminal});
-    });
-  }).select('-__v');
-}
+//       response.json({message: 'Criminal successfully updated', criminal: criminal});
+//     });
+//   }).select('-__v');
+// }
 
-function removeCriminal(request, response) {
-  var id = request.params.id;
+// function removeCriminal(request, response) {
+//   var id = request.params.id;
 
-  Criminal.remove({_id: id}, function(error) {
-    if(error) response.json({message: 'Could not delete criminal b/c:' + error});
+//   Criminal.remove({_id: id}, function(error) {
+//     if(error) response.json({message: 'Could not delete criminal b/c:' + error});
 
-    response.json({message: 'Criminal successfully deleted'});
-  }).select('-__v');
-}
+//     response.json({message: 'Criminal successfully deleted'});
+//   }).select('-__v');
+// }
 
-module.exports = {
-  getAll: getAll,
-  createCriminal: createCriminal,
-  getCriminal: getCriminal,
-  updateCriminal: updateCriminal,
-  removeCriminal: removeCriminal
-}
+// module.exports = {
+//   getAll: getAll,
+//   createCriminal: createCriminal,
+//   getCriminal: getCriminal,
+//   updateCriminal: updateCriminal,
+//   removeCriminal: removeCriminal
+// }
--- frontend/js/moviesController.js
+++ frontend/js/moviesController.js
@@ -10,12 +10,20 @@ angular.module('FlicPic', [])
 
     //get all of the movies
     this.all = [];
+
+    this.getMovies = getMovies;
+
+    this.title = "FlicPic"
     // this.addMovie = addMovie;
     // this.newMovie = {};
+
+    // get all of the genres to populate them in an array
     self.genres = [];
 
     function getMovies(queryString) {
       var url = 'https://api.themoviedb.org/3/search/movie?query=' + queryString + '&api_key=213e6d38b03c7af40fb82d70ad6f0139'
+      console.log(url);
+
       $http
         .get()//api url
         .then(function(res) {
@@ -24,40 +32,40 @@ angular.module('FlicPic', [])
         });
     }
 
-    function getGenres() {
-      $http
-        .get('https://api.themoviedb.org/3//genre/movie/list?api_key=213e6d38b03c7af40fb82d70ad6f0139') //https://api.themoviedb.org/3/genre/movie/list?api_key=213e6d38b03c7af40fb82d70ad6f0139
-        //genre/id/movies gets a list of movies for a particular genre by id
-        .then(function(res) {
-          console.log(res);
-          self.genres = res.data.genres;
-        });
-    }
+    // function getGenres() {
+    //   $http
+    //     .get('https://api.themoviedb.org/3//genre/movie/list?api_key=213e6d38b03c7af40fb82d70ad6f0139') //https://api.themoviedb.org/3/genre/movie/list?api_key=213e6d38b03c7af40fb82d70ad6f0139
+    //     //genre/id/movies gets a list of movies for a particular genre by id
+    //     .then(function(res) {
+    //       console.log(res);
+    //       self.genres = res.data.genres;
+    //     });
+    // }
 
-    function getActors(queryString) {
-      var url = 'https://api.themoviedb.org/3/search/person?query=' + queryString + '&api_key=213e6d38b03c7af40fb82d70ad6f0139'
-      $http
-        .get()//api url
-        .then(function(res) {
-          console.log(res);
-          self.all = res.data.movies;
-        });
-    }
+    // function getActors(queryString) {
+    //   var url = 'https://api.themoviedb.org/3/search/person?query=' + queryString + '&api_key=213e6d38b03c7af40fb82d70ad6f0139'
+    //   $http
+    //     .get()//api url
+    //     .then(function(res) {
+    //       console.log(res);
+    //       self.all = res.data.movies;
+    //     });
+    // }
 
-    function getYears(queryString) {
-      var url = 'https://api.themoviedb.org/3/discover/movie?primary_release_year=' + queryString + '&api_key=213e6d38b03c7af40fb82d70ad6f0139'
-      $http
-        .get()//api url
-        .then(function(res) {
-          console.log(res);
-          self.all = res.data.movies;
-        });
-    }
+    // function getYears(queryString) {
+    //   var url = 'https://api.themoviedb.org/3/discover/movie?primary_release_year=' + queryString + '&api_key=213e6d38b03c7af40fb82d70ad6f0139'
+    //   $http
+    //     .get()//api url
+    //     .then(function(res) {
+    //       console.log(res);
+    //       self.all = res.data.movies;
+    //     });
+    // }
 
   
 
-    getMovies();
-    getGenres();
-    getActors();
-    getYears();
+    // getMovies();
+    // getGenres();
+    // getActors();
+    // getYears();
   }
